deb5348aab947d19a99b2ec11de169cf44dced43
RP Cai <8058474+vtility@user.noreply.gitee.com>
2021-06-25 20:34:11 +0800

    trying more

--- C/HelloWorld/helloworld.c
+++ C/HelloWorld/helloworld.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 int main(){
     printf("hello world\n");
-    int i;
+    int i,z;
 
     for(i=1; i<=9; i=i+1){ //用分号隔开，终止条件应该是“范围”。
-        printf("%8f\n",i); //不能直接输出数字，不能用f打印整形.
+        printf("%d\n",i); //不能直接输出数字，不能用f打印整形.
     }
+    printf("\n");
+    //z = ++i;
+    //printf("%d\n",z);//output:11
+    //printf("\n");
+    //z = i++;
+    printf("%d\n",i);
+    printf("%d\n",i);//output:10 循环之后i又完成了一次自增，但不符合循环条件故没有打印。
+
+
 }
