1622a79b2c83163e6403a1b96dcf8506ef6c71a9
RP Cai <8058474+vtility@user.noreply.gitee.com>
2021-06-25 20:06:13 +0800

    try "for" in helloworld

--- C/HelloWorld/helloworld.c
+++ C/HelloWorld/helloworld.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
 int main(){
-    printf("hello world");
+    printf("hello world\n");
+    int i;
+
+    for(i=1; i<=9; i=i+1){ //用分号隔开，终止条件应该是“范围”。
+        printf("%8f\n",i); //不能直接输出数字，不能用f打印整形.
+    }
 }
